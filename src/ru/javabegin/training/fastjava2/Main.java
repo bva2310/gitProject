42c30bd403160ebccb2cf64002d1a603a1f2ef4c
gorovtsov <garavek94@gmail.com>
2021-01-10 18:08:06 +0300

    Branched

--- src/ru/javabegin/training/fastjava2/Main.java
+++ src/ru/javabegin/training/fastjava2/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Alalalalala");
         System.out.println("Hello world!!!");
+        System.out.println("Hello from second branch");
     }
 }
